b6228817432259b5866cf909572a20fcc57eff98
albrtovicente <albervifetandem@gmail.com>
2024-04-22 13:07:04 +0200

    actualizar

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,19 @@ if (audioPlayer.paused) {
     audioPlayer.pause();
 }
 });
+
+// // Boton arriba
+// (function() {
+//     //a partir de que punto del scroll vertical de la ventana mostrará el botón
+//     const ishow = 115
+//     const $divtop = document.getElementById("div-totop")
+//     window.addEventListener("scroll", function() {
+//         if(document.documentElement.scrollTop > ishow){
+//             $divtop.style.display = "inherit"
+//         }
+//         else {
+//             $divtop.style.display = "none"
+//         }
+//     })
+// })()
+
